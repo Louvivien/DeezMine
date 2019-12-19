a8269b74bd8ab9d17234b8ba07ffffed89373ae4
Sly <53995394+SlyBcDev@users.noreply.github.com>
2019-12-19 10:38:47 +0100

    rectif

--- Deezmine mobile App/config.js
+++ Deezmine mobile App/config.js
@@ -1,7 +1,7 @@
 import deezmineContractABI from './deezmineContractABI.json';
 import Web3 from 'web3';
 
-export const CONTRACTADDRESS = '0x96b4f3bb3460861cc3c653f3789245de9eff0ab3';
+export const CONTRACTADDRESS = '0x0df5640b3da3fe9f2740e364c66abb93aeb75476';
 
 export const web3 = new Web3(
   new Web3.providers.HttpProvider(

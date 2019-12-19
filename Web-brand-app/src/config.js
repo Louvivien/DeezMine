7a36bacde276f48efd0bd946ed914e0eca54c620
Sly <53995394+SlyBcDev@users.noreply.github.com>
2019-12-19 10:37:33 +0100

    rectif

--- Web-brand-app/src/config.js
+++ Web-brand-app/src/config.js
@@ -1,7 +1,7 @@
 import Web3 from "web3";
 import deezmineContractABI from "./deezmineContractABI.json";
 
-export const CONTRACT_ADDRESS = "0x96b4f3bb3460861cc3c653f3789245de9eff0ab3";
+export const CONTRACT_ADDRESS = "0x0df5640b3da3fe9f2740e364c66abb93aeb75476";
 
 export const web3 = new Web3(
   Web3.givenProvider || "kovan.infura.io/v3/da3c3685867d470b8b9a8a6dffd6ffd0"

55b8008834e0a1af9c8da99ff7d7e270e61ca1b0
Sly <53995394+SlyBcDev@users.noreply.github.com>
2020-01-29 15:16:41 +0100

    update

--- Web-brand-app/src/App.js
+++ Web-brand-app/src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GenerateKeys from "./Component/GenerateKeys";
 import Home from "./Component/Home";
 import User from "./Component/User";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/brandtools" component={GenerateKeys} />
-          <Route exact path="/user" component={User} />
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/brandtools" component={GenerateKeys} />
+            <Route exact path="/user" component={User} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
